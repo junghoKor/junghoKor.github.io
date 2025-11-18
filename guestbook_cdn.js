c8048a35a2350a427a6cafbf0513055bad45ea92
Jungho Ryu <46273970+junghoKor@users.noreply.github.com>
2025-11-19 02:44:21 +0900

    Add 'set' function and update storage bucket URL

--- guestbook_cdn.js
+++ guestbook_cdn.js
@@ -3,21 +3,25 @@ import {
   getDatabase,
   ref,
   push,
+  set,
   onValue,
   remove
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
+// Firebase 설정
 const firebaseConfig = {
   apiKey: "AIzaSyCGNeDfVZihhr5_Wva0BcPWA0RC2TU9ZR8",
   authDomain: "junghokor-guestbook.firebaseapp.com",
   databaseURL: "https://junghokor-guestbook-default-rtdb.firebaseio.com",
   projectId: "junghokor-guestbook",
-  storageBucket: "junghokor-guestbook.firebasestorage.app",
+  storageBucket: "junghokor-guestbook.appspot.com",
   messagingSenderId: "1060425715746",
   appId: "1:1060425715746:web:775a1f1027caedbc2dfa7b"
 };
 
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, push, onValue, remove };
+// 필요한 기능 export
+export { db, ref, push, set, onValue, remove };
